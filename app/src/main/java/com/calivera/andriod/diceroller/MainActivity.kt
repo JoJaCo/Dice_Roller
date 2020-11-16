7d58e6376b8719ad3a1ee9a6ba4023f81451b7d8
JoJaCo <contrerasjorge226@gmail.com>
2020-11-16 02:20:26 -0800

    applied kotlin-android-extensions and started on the listeners

--- app/src/main/java/com/calivera/andriod/diceroller/MainActivity.kt
+++ app/src/main/java/com/calivera/andriod/diceroller/MainActivity.kt
@@ -2,10 +2,16 @@ package com.calivera.andriod.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        roll_dice.setOnClickListener {
+            Toast.makeText(this,"button is clisked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
